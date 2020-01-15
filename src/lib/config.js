3ba55722b05b89b14d34e07a9cc166642cee9ea3
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2020-01-15 18:37:59 -0300

    init files

---
+++ src/lib/config.js
@@ -0,0 +1,35 @@
+import WPAPI from 'wpapi';
+
+let config = {
+    body: document.querySelector( 'body' ),
+    siteTitle: document.querySelector( '.archive-title' ),
+    siteDescription: document.querySelector( '.site-description' ),
+    title: document.querySelector( '.title-editor' ),
+    content: document.querySelector( '.entry-content' ),
+    menuContainer: document.querySelector( '#mainNav ul' ),
+    articleContainer: document.querySelector( 'main#main' ),
+    sidebar: document.querySelector( '#secondary' ),
+    apiRoot: 'https://to-demo.lsdev.biz/wp-json',
+};
+
+config.page404 = {
+    type: '404',
+    title: {
+        rendered: '404 Error.'
+    },
+    content: {
+        rendered: '<p>This Page was not found</p>'
+    },
+    link: '/',
+    _embedded: {
+        author: [
+            {
+                name: 'Admin'
+            }
+        ]
+    }
+};
+
+config.wp = new WPAPI( { endpoint: config.apiRoot } );
+
+export default config;
